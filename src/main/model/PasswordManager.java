786bb29fd9ee97228b6d2e87256206e38f7f064e
Sadia <sadiakd@students.cs.ubc.ca>
2022-10-02 23:15:58 -0700

    Added a few methods

--- src/main/model/PasswordManager.java
+++ src/main/model/PasswordManager.java
@@ -4,17 +4,34 @@ import java.util.ArrayList;
 
 // PasswordManager represents the passwords that have been added
 public class PasswordManager {
+    private final ArrayList<Password> passwords;
 
     //REQUIRES:
     //MODIFIES:
     //EFFECTS:
     public PasswordManager() {
-        ArrayList<Password> passwords = new ArrayList<Password>();
+        passwords = new ArrayList<>();
     }
 
-    public void addPassword() {
-
+    //REQUIRES:
+    //MODIFIES:
+    //EFFECTS:
+    public void addPassword(Password password) {
+        passwords.add(password);
     }
 
+    //REQUIRES:
+    //MODIFIES:
+    //EFFECTS:
+    public void removePassword(Password password) {
+        passwords.remove(password);
+    }
+
+    //REQUIRES:
+    //MODIFIES:
+    //EFFECTS:
+    //public int countPasswords(ArrayList<Password>) {
+    //    passwords.size();
+    //}
 
 }
