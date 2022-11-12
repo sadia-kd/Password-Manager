12c8f3674de3f2f2a971fe89704d3b26807229d5
Sadia <sadiakd@students.cs.ubc.ca>
2022-11-11 20:48:34 -0800

    committing gui: layout done - added functionality (not done)

--- src/main/ui/gui/PasswordManagerGui.java
+++ src/main/ui/gui/PasswordManagerGui.java
@@ -2,34 +2,45 @@ package ui.gui;
 
 import model.Account;
 import model.PasswordManager;
+import persistence.JsonReader;
+import persistence.JsonWriter;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class PasswordManagerGui extends JFrame implements ActionListener {
     private Account account;
     private PasswordManager passwordManager;
 
-    private static final int WIDTH = 650;
-    private static final int HEIGHT = 400;
+    private static final String JSON_STORE = "./data/PasswordManagerGUI.json";
+
+    private static final int WIDTH = 500;
+    private static final int HEIGHT = 500;
 
     private JFrame frame;
-    private JPanel panel1;
-    private JPanel panel2;
+
+    private JPanel panel;
+    private JScrollPane scrollPane;
+
+    private JTable table;
+    private DefaultTableModel defaultTableModel;
 
     private JButton button1;
     private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JLabel label;
 
     private JMenuBar menuBar;
-    private JMenu menu1;
-    private JMenu menu2;
+    private JMenu menu;
+
+    private JMenuItem item1;
+    private JMenuItem item2;
 
     private JLabel l1;
     private JLabel l2;
@@ -39,123 +50,253 @@ public class PasswordManagerGui extends JFrame implements ActionListener {
     private JTextField t2;
     private JTextField t3;
 
-    private JTabbedPane tabbedPane;
+    private JLabel l4;
+    private JLabel l5;
+    private JTextField t4;
+    private JTextField t5;
 
-    // TODO
-    //
+    private JsonWriter jsonWriter;
+    private JsonReader jsonReader;
+
+    private Border br = BorderFactory.createLineBorder(Color.black);
+
     public PasswordManagerGui() {
         passwordManager = new PasswordManager();
+        jsonWriter = new JsonWriter(JSON_STORE);
+        jsonReader = new JsonReader(JSON_STORE);
 
-        // Create the window
-        frame = new JFrame("Password Manager");
-        panel2 = new JPanel();
-        panel2.setLayout(new GridLayout(1, 0));
-        panel2.setBackground(Color.lightGray);
-
-        panel1 = new JPanel();
-        panel1.setLayout(new GridLayout(0, 6));
-        panel1.setBackground(Color.white);
-
-        tabbedPane = new JTabbedPane();
-        tabbedPane.add(panel1);
-        tabbedPane.add(panel2);
-
-        setThings();
-        initialButtons();
-        initalLabels();
+        frame = new JFrame();
+        setUp();
         setMenu();
+        addPanel();
+        frame.setVisible(true);
     }
 
-    private void setThings() {
-        frame.setSize(WIDTH, HEIGHT);
-        //frame.add(panel1, BorderLayout.WEST);
-        //frame.add(panel2, BorderLayout.EAST);
-        frame.add(tabbedPane);
-        frame.setVisible(true);
+    private void setUp() {
+        frame.setTitle("PASSWORD MANAGER");
+        // TODO
+        // what should i use
+        //frame.setLayout(new GridLayout());
+        frame.setLayout(null);
+        frame.setBounds(0,0, WIDTH + 90, HEIGHT + 60);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
     }
+
 
     private void setMenu() {
         //Build menu bar.
         menuBar = new JMenuBar();
-        menu1 = new JMenu("Load File");
-        menu2 = new JMenu("Save File");
+        menu = new JMenu("File");
 
-        menuBar.add(menu1);
-        menu1.setMnemonic(KeyEvent.VK_A);
-        //menu1.getAccessibleContext().setAccessibleDescription(
-        //        "The only menu in this program that has menu items");
+        item1 = new JMenuItem("save");
+        item1.addActionListener(this);
+        item2 = new JMenuItem("load");
+        item2.addActionListener(this);
 
-        menuBar.add(menu2);
-        menu2.setMnemonic(KeyEvent.VK_A);
-        //menu2.getAccessibleContext().setAccessibleDescription(
-        //        "The only menu in this program that has menu items");
+        menu.add(item1);
+        menu.add(item2);
+
+        menuBar.add(menu);
+        menu.setMnemonic(KeyEvent.VK_A);
+
+        menu.addActionListener(this);
 
         frame.add(menuBar);
         frame.setJMenuBar(menuBar);
 
     }
 
-    private void initialButtons() {
+
+    private void addPanel() {
+        //Creating a JPanel for the JFrame
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        panel.setBorder(BorderFactory.createLineBorder(Color.red));
+        // changing the background color of the panel
+        panel.setBackground(Color.lightGray);
+        panel.setBounds(10,10,(WIDTH / 5) * 3,HEIGHT - 20);
+        setButtons();
+
+        scrollPane = new JScrollPane();
+        scrollPane.setLayout(null);
+        scrollPane.setBorder(br);
+        scrollPane.setBackground(Color.GRAY);
+        scrollPane.setBounds(320,10,(WIDTH / 5) * 3 - 50,HEIGHT / 8);
+
+        //adding a label element to the panel
+        JLabel label = new JLabel("Accounts");
+        //label.setSize(90, 25);
+        //label.setBorder(BorderFactory.createLineBorder(Color.red));
+        label.setBounds(10,10,100,30);
+        scrollPane.add(label);
+
+        JLabel countLabel = new JLabel("Total Accounts Saved: " + passwordManager.getCount());
+        countLabel.setBounds(10,30,200,20);
+        scrollPane.add(countLabel);
+
+        //adding to the JFrame
+        frame.add(panel);
+        frame.add(scrollPane);
+        setTable();
+    }
+
+    private void setButtons() {
         button1 = new JButton("ADD");
-        panel1.add(button1);
+        addEntries();
+        panel.add(button1);
         button1.addActionListener(this);
 
-        l1 = new JLabel("Application");
-        l2 = new JLabel("Username");
-        l3 = new JLabel("Password");
+        button2 = new JButton("REMOVE");
+        //removeEntries();
+        panel.add(button2);
+        button2.addActionListener(this);
 
-        t1 = new JTextField(10);
+    }
+
+    private void setTable() {
+        Object [] [] data = {
+
+        };
+
+        String[] columnNames = {"Application:",
+                "User:",
+                "Pass:"};
+
+        defaultTableModel = new DefaultTableModel(data, columnNames);
+        table = new JTable(defaultTableModel);
+        table.setBackground(Color.pink);
+        table.setBorder(BorderFactory.createLineBorder(Color.red, 2));
+        table.setFillsViewportHeight(true);
+
+        JScrollPane pane2 = new JScrollPane(table);
+        pane2.setBounds(320,20 + HEIGHT / 8,(WIDTH / 5) * 3 - 50, HEIGHT / 2 + 160);
+        frame.add(pane2);
+    }
+
+
+    private void addEntries() {
+        l1 = new JLabel("Application: ");
+        t1 = new JTextField(50);
+
+        l2 = new JLabel("Username: ");
         t2 = new JTextField(10);
+
+        l3 = new JLabel("Password: ");
         t3 = new JTextField(10);
 
-        frame.add(l1);
-        frame.add(t1);
-        frame.add(l2);
-        frame.add(t2);
-        frame.add(l3);
-        frame.add(t3);
+        setNull();
 
-//
-//        button2 = new JButton("REMOVE");
-//        panel1.add(button2);
-//
-//        button3 = new JButton("VIEW");
-//        panel1.add(button3);
-//
-//        button4 = new JButton("TOTAL");
-//        button4.addActionListener(this);
-//        panel1.add(button4);
-
+        panel.add(l1);
+        panel.add(t1);
+        panel.add(l2);
+        panel.add(t2);
+        panel.add(l3);
+        panel.add(t3);
     }
 
-    private void initalLabels() {
-        label = new JLabel("Saved Accounts");
-        panel2.add(label);
+    private void removeEntries() {
+//        l4 = new JLabel("Application Name to remove: ");
+//        t4 = new JTextField(10);
+//        l5 = new JLabel("Username to remove: ");
+//        t5 = new JTextField(10);
+//        panel.add(l4);
+//        panel.add(t4);
+//        panel.add(l5);
+//        panel.add(t5);
     }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        Object object = e.getSource();
-        if (object == button1) {
+        if (e.getSource() == button1) {
             addPassword();
-        } else if (object == button4) {
-            total();
+        } else if (e.getSource() == button2) {
+            removePassword();
+        } else if (e.getSource() == item1) {
+            savePasswordManager();
+        } else if (e.getSource() == item2) {
+            loadPasswordManager();
         }
     }
+
 
     private void addPassword() {
         String app = t1.getText();
         String u = t2.getText();
         String p = t2.getText();
         account = new Account(app, u, p);
-        passwordManager.addAccount(account);
+
+        if (passwordManager.containsAccount(app, u)) {
+            JOptionPane.showMessageDialog(null, "An account like this already exists!",
+                    "Problem!", JOptionPane.INFORMATION_MESSAGE);
+            setNull();
+        } else {
+            passwordManager.addAccount(account);
+            setNull();
+            System.out.println("\nThis account has been saved to your Password Manager!!!");
+            Object[] newRow = {app, u, p};
+            defaultTableModel.addRow(newRow);
+        }
+
     }
 
-    private void total() {
-        JLabel label = new JLabel("The total passwords you have saved are: " + passwordManager.getCount());
-        frame.add(label);
+    private void removePassword() {
+        String app = t1.getText();
+        String u = t2.getText();
+        String p = t2.getText();
+        if (passwordManager.getCount() == 0) {
+            System.out.println("You do not have any passwords saved ... ");
+            setNull();
+            JOptionPane.showMessageDialog(null, "You have 0 passwords saved",
+                    "Problem!", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            if (passwordManager.removeAccount(app, u)) {
 
+                Object[] row = {app, u, p};
+                // TODO
+                // fix
+                //defaultTableModel.removeRow();
+                defaultTableModel.addRow(row);
+
+                System.out.println("\nThis account has been removed!");
+                setNull();
+            } else {
+                System.out.println("\nThis Password Manager does not contain this account!");
+                setNull();
+                JOptionPane.showMessageDialog(null, "No such account exists",
+                        "Problem!", JOptionPane.INFORMATION_MESSAGE);
+            }
+        }
+    }
+
+    private void setNull() {
+        t1.setText(null);
+        t2.setText(null);
+        t3.setText(null);
+    }
+
+    private void savePasswordManager() {
+        try {
+            jsonWriter.open();
+            jsonWriter.write(passwordManager);
+            jsonWriter.close();
+
+            JOptionPane.showMessageDialog(null, "Saved!",
+                    "Congrats!", JOptionPane.INFORMATION_MESSAGE);
+
+        }  catch (FileNotFoundException e) {
+            System.out.println("Unable to write to file: " + JSON_STORE);
+        }
+    }
+
+    private void loadPasswordManager() {
+        try {
+            passwordManager = jsonReader.read();
+            System.out.println("Loaded Password Manager from " + JSON_STORE);
+        } catch (IOException e) {
+            System.out.println("Unable to read from file: " + JSON_STORE);
+        }
     }
 
 }
